31fcccca00eeb350a8a4e3932a6cce2385da7dec
onurergun <onrrgn@gmail.com>
2022-05-30 00:51:14 +0300

    Initialize backend Sprint Boot module

---
+++ finobs-be/src/main/java/com/onurergun/finobsbe/FinobsBeApplication.java
@@ -0,0 +1,13 @@
+package com.onurergun.finobsbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinobsBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinobsBeApplication.class, args);
+    }
+
+}
---
+++ finobs-be/src/test/java/com/onurergun/finobsbe/FinobsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onurergun.finobsbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinobsBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
