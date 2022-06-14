f39db437d87c59a0f8b4d9df1ebc2ef0a4573b31
onurergun <onrrgn@gmail.com>
2022-06-14 22:56:00 +0300

    feat(fe-navbar): navbar added to fe
    Navbar added for front end
    Favicon updated
    Body background changed with
    react-helmet
    
    [skip ci]

--- finobs-fe/src/App.js
+++ finobs-fe/src/App.js
@@ -1,10 +1,17 @@
 import SignUp from "./pages/SignUp";
+import NavBar from "./components/NavBar";
+import {Helmet} from "react-helmet"
+
 import "./app.css";
 
 function App() {
   return (
-    <div className="parent-container">
-      <SignUp></SignUp>
+    <div>
+        <Helmet>
+            <style>{'body { background-color: #F8F9FA; }'}</style>
+        </Helmet>
+        <NavBar></NavBar>
+        <SignUp></SignUp>
     </div>
   );
 }
