fe0855fb919e7655b35dca24e7ad51ec99bfb892
onurergun <onrrgn@gmail.com>
2022-06-23 18:10:17 +0300

    feat(fe-routing): routing added to app entry point
    routing added to app entry point
    Error, Home, Login page templates
    created
    
    [skip ci]

--- finobs-fe/src/App.js
+++ finobs-fe/src/App.js
@@ -1,21 +1,30 @@
 import "./app.css";
 import {Helmet} from "react-helmet"
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 
 import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+import Error from "./pages/Error";
 
 
 function App() {
   return (
-    <div>
+    <Router>
         <Helmet>
             <style>{'body { background-color: #F8F9FA; }'}</style>
         </Helmet>
         <NavBar></NavBar>
-        <SignUp></SignUp>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<Error />} />
+        </Routes>
         <Footer></Footer>
-    </div>
+    </Router>
   );
 }
 
