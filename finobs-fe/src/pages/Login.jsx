ab2485604190ec78be965673244138b3b0f66588
onurergun <onrrgn@gmail.com>
2022-06-25 13:36:39 +0300

    feat(fe-login): login page added
    Login page added and service methods implemented
    
    [skip ci]

--- finobs-fe/src/pages/Login.jsx
+++ finobs-fe/src/pages/Login.jsx
@@ -1,9 +1,82 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-function Login (props) {
+import "./login.css"
+import FormInput from "../components/FormInput";
+import FormInputPassword from "../components/FormInputPassword";
+import FormButton from "../components/FormButton";
+import AuthService from "../services/AuthService";
+import Alert from "../components/Alert";
+
+function Login(props) {
+    const [stateValues, setStateValues] = useState({
+        userName: "",
+        password: ""
+    });
+
+    const [stateError, setStateError] = useState("");
+
+    const [stateProcessing, setStateProcessing] = useState(false);
+
+    const errorMessageDefault = "Invalid username or password";
+
+    function onInputChange(event) {
+        const {name, value} = event.target;
+
+        setStateValues(prev => {
+            return {
+                ...prev,
+                [name]: value
+            };
+        });
+
+        setStateError(undefined);
+    }
+
+    function onButtonClick(event) {
+        console.log("Login Button clicked");
+        console.log(stateValues);
+
+        event.preventDefault();
+        setStateProcessing(true);
+        AuthService.login(stateValues)
+            .then(response => {
+                if (response.status === 200) {
+                    console.log("User login request successful");
+                } else {
+                    console.log("User login request failed: " + response.status +" - " +  response.statusText);
+                    setStateError(errorMessageDefault);
+                    setStateProcessing(false);
+                }
+            })
+            .catch(err => {
+                console.log("User login request internal error" + err);
+                setStateError(errorMessageDefault + " INVALID");
+                setStateProcessing(false);
+            });
+    }
+
     return (
-        <div>
-            <h1>LOGIN PAGE</h1>
+        <div className="container mt-5">
+            <h1 className="login-heading">Login</h1>
+            <form>
+                <FormInput labelText={"User Name"}
+                           inputType={"text"}
+                           inputId={"userName"}
+                           inputName={"userName"}
+                           errorMessage={undefined}
+                           required={true}
+                           inputOnChange={onInputChange}/>
+                <FormInputPassword labelText={"Password"}
+                                   inputId={"password"}
+                                   inputName={"password"}
+                                   errorMessage={undefined}
+                                   required={true}
+                                   inputOnChange={onInputChange}/>
+                {stateError && <Alert message={stateError}></Alert>}
+                <FormButton label={"Login"}
+                            processing={stateProcessing}
+                            onClick={onButtonClick}/>
+            </form>
         </div>
     );
 }
